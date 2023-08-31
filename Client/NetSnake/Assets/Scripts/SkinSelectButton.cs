be159db102bd83983313ddf03c1d924d2f99b3e9
dev.ips <ilpyatkov@gmail.com>
2023-08-31 23:59:21 +0300

    TODO

--- Client/NetSnake/Assets/Scripts/SkinSelectButton.cs
+++ Client/NetSnake/Assets/Scripts/SkinSelectButton.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class SkinSelectButton : MonoBehaviour
 {
+    // TODO disable selected buttons from other clients menu
     public void SelectSkin(int value) {
         MultiplayerManager.Instance.Connection(value);
     }
